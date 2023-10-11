d2ff4853ac9b0f090dd1294e03d3e3897e23e568
Emiru David Egwell <david.egwell@EmongLoOtuboi.local>
2023-10-11 07:24:34 +0300

    Created Pages and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import{BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Offers from "./pages/Offers"
+import Profile from "./pages/Profile"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import ForgotPassword from "./pages/ForgotPassword"
 
 function App() {
   return (
     <>
-    <h1>Uganda is Beautiful</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element= {<Home/>}></Route>
+        <Route path="/offers" element= {<Offers/>}></Route>
+        <Route path="/profile" element= {<Profile/>}></Route>
+        <Route path="/sign-in" element= {<SignIn/>}></Route>
+        <Route path="/sign-up" element= {<SignUp/>}></Route>
+        <Route path="/forgot-password" element= {<ForgotPassword/>}></Route>
+      </Routes>
+    </Router>
     </>
   );
 }
